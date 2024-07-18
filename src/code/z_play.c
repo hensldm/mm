58c2c8248140d9b5044b65b6d680004142b58945
Derek Hensley <hensley.derek58@gmail.com>
2024-07-18 08:50:14 -0700

    Frame Advance

--- src/code/z_play.c
+++ src/code/z_play.c
@@ -938,7 +938,7 @@ void Play_UpdateMain(PlayState* this) {
 
     Actor_SetMovementScale(this->state.framerateDivisor);
 
-    if (FrameAdvance_Update(&this->frameAdvCtx, &input[1])) {
+    if (FrameAdvance_Update(&this->frameAdvCtx, &input[0])) {
         if ((this->transitionMode == TRANS_MODE_OFF) && (this->transitionTrigger != TRANS_TRIGGER_OFF)) {
             this->transitionMode = TRANS_MODE_SETUP;
         }
